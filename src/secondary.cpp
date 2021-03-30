3ebef870808823b57132c68a77da77b8c33f9bea
Sijun Tan <sijuntan@yeah.net>
2021-03-30 17:55:02 -0400

    .

--- src/secondary.cpp
+++ src/secondary.cpp
@@ -1146,13 +1146,13 @@ void selectNetwork(string network, string dataset, string security, NeuralNetCon
 			MaxpoolConfig* l4 = new MaxpoolConfig(7,7,256,3,2,MINI_BATCH_SIZE);
 			ReLUConfig* l5 = new ReLUConfig(3*3*256,MINI_BATCH_SIZE);	
 
-			CNNConfig* l6 = new CNNConfig(5,5,256,384,3,1,1,MINI_BATCH_SIZE);
-			ReLUConfig* l7 = new ReLUConfig(5*5*384,MINI_BATCH_SIZE);
-			CNNConfig* l8 = new CNNConfig(5,5,384,384,3,1,1,MINI_BATCH_SIZE);
-			ReLUConfig* l9 = new ReLUConfig(5*5*384,MINI_BATCH_SIZE);
+			CNNConfig* l6 = new CNNConfig(3,3,256,384,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l7 = new ReLUConfig(3*3*384,MINI_BATCH_SIZE);
+			CNNConfig* l8 = new CNNConfig(3,3,384,384,3,1,1,MINI_BATCH_SIZE);
+			ReLUConfig* l9 = new ReLUConfig(3*3*384,MINI_BATCH_SIZE);
 			
-			CNNConfig* l10 = new CNNConfig(5,5,384,256,3,1,1,MINI_BATCH_SIZE);
-			MaxpoolConfig* l11 = new MaxpoolConfig(5,5,256,2,1,MINI_BATCH_SIZE);
+			CNNConfig* l10 = new CNNConfig(3,3,384,256,3,1,1,MINI_BATCH_SIZE);
+			MaxpoolConfig* l11 = new MaxpoolConfig(3,3,256,2,1,MINI_BATCH_SIZE);
 			ReLUConfig* l12 = new ReLUConfig(2*2*256,MINI_BATCH_SIZE);	
 
 			FCConfig* l13 = new FCConfig(2*2*256,MINI_BATCH_SIZE,1024);
